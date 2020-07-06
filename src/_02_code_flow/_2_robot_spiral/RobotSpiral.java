ad8f3ebf7b572727e29c43d2645fe021b9903663
BakuGone <riana.acquista@gmail.com>
2020-07-06 15:26:59 -0700

    Update RobotSpiral.java

--- src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
+++ src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
@@ -12,24 +12,27 @@ public class RobotSpiral {
 	public static void main(String[] args) {
 		
 		// Create a new Robot
+		Robot fred = new Robot();
 		
-		// Set your robot's pen down 
-		
-		// SPEED. Set the robot to go at max speed (100)
-		
+		fred.penDown(); 
+		fred.setSpeed(100);
         // LOOP. Use a for loop to repeat ALL the following lines of code 50 times. 
-		
+		for(int i = 1; i < 51; i++) {
 				// Change the robot pen color to random
-			
+				fred.setRandomPenColor();
 				// DRAW.    Move the robot (5*i) pixels
 				//          "i" is the variable in the for loop	
+				fred.move(5*i);
 			
 				// TURN.    Turn the robot (360/7) degrees to the right
+				fred.turn(360/7);
 			
 				// Change the robot pen width to (i)
-
+				fred.setPenWidth(i);
 		
 		// Check the pattern against the picture in the recipe. If it matches, you are done!
+		
+		}
 		
 	}
 }
