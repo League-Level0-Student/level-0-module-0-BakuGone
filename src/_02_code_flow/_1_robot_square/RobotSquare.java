015a233a372124df186cc2bb72e7bab7e2791f94
BakuGone <riana.acquista@gmail.com>
2020-07-06 15:26:56 -0700

    Update RobotSquare.java

--- src/_02_code_flow/_1_robot_square/RobotSquare.java
+++ src/_02_code_flow/_1_robot_square/RobotSquare.java
@@ -14,25 +14,14 @@ public class RobotSquare {
  
     	// This code makes a new Robot
     	Robot rob = new Robot();
+    	
+    	rob.penDown();
+    	rob.setSpeed(10);
 
-        // Put the robot's pen down so it can draw
-		//        Use the     rob.penDown();   command.
-
-        // SPEED. Make the robot move quickly.
-		//        Use the     rob.setSpeed();   command.
-
-
-        // DRAW.  Draw a line
-    	//		Use the     rob.move(); command
-
-
-		// TURN.  Turn 90 degrees
-    	//      Use the     rob.turn();  command
-
-
-        // LOOP.  Now put the DRAW and TURN code into a for loop to repeat 4 times.
-		//        Did your Robot draw a square?
-
-
+        for (int i = 1; i < 5; i++) {
+        	rob.move(100);
+        	rob.turn(90);
+    	
+        }
     }
 }
