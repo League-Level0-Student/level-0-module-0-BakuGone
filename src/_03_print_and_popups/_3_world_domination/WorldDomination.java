ade299d1e1fffa00c3c0184b2f9d46e0aae75c0d
BakuGone <riana.acquista@gmail.com>
2020-07-06 15:27:18 -0700

    Update WorldDomination.java

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,9 +8,16 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String code = JOptionPane.showInputDialog("Do you know how to write code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+		if (code != "yes") {
+			JOptionPane.showMessageDialog(null, "You will rule the world!");
+			
+		}
+		else if (code != "no") {
+			JOptionPane.showMessageDialog(null, "Sign up for classes at The Leauge for /n"
+					+ "world domination ;D");
+		}
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
 	}
